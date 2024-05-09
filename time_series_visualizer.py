a91c3c65094b56e31a5c4558aa85b7dfb9d7c6db
Zhan Shu <zhan.shu@rwth-aachen.de>
2024-05-09 18:02:51 +0200

    complete line chart

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -5,18 +5,21 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = pd.read_csv('fcc-forum-pageviews.csv', )
+df = pd.read_csv('fcc-forum-pageviews.csv', parse_dates=[0], index_col=[0])
 
 # Clean data
-df = None
+mask = (df['value'] > df['value'].quantile(0.025)) & (df['value'] < df['value'].quantile(0.975))
+df = df[mask]
 
 
 def draw_line_plot():
     # Draw line plot
+    fig, ax = plt.subplots(figsize=(32, 10))
 
-
-
-
+    ax.plot(df.index, df['value'], color='red', linewidth=3)
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Page Views')
+    ax.set_title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
 
     # Save image and return fig (don't change this part)
     fig.savefig('line_plot.png')
